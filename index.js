fc43000ee28bc335d1e706950c7c09aa3d0310fa
qbixmex <qbixmex@gmail.com>
2021-05-06 22:01:48 -0500

    Express Instalado

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello ExpressJS');
+const express = require("express");
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get("/", ( request, response ) => {
+  console.log("Se requiere el slash");
+  response.json({
+    ok: true
+  });
+});
+
+// Escuchar Peticiones
+app.listen( 8000, () => {
+  console.log(`Server running on port: ${ 8000 }`);
+});
