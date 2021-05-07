26300de2fb68108d3892517ffa4a55948510ce32
qbixmex <qbixmex@gmail.com>
2021-05-06 22:21:07 -0500

    Variables de entorno y carpeta pública

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
+require("dotenv").config();
 
 // Crear el servidor de express
 const app = express();
 
+// Directorio Público
+app.use( express.static("public") );
+
 // Rutas
-app.get("/", ( request, response ) => {
-  console.log("Se requiere el slash");
-  response.json({
-    ok: true
-  });
-});
+// app.get("/", ( request, response ) => {
+//   console.log("Se requiere el slash");
+//   response.json({
+//     ok: true
+//   });
+// });
 
 // Escuchar Peticiones
-app.listen( 8000, () => {
-  console.log(`Server running on port: ${ 8000 }`);
+app.listen( process.env.PORT, () => {
+  console.log(`Server running on port: ${ process.env.PORT }`);
 });
