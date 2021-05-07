d44b72c0ef60610a178eda7467c3ba7e1feb5170
qbixmex <qbixmex@gmail.com>
2021-05-06 23:36:12 -0500

    Recuperar información de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,33 @@
 const { response } = require("express");
 
 const crearUsuario = (request, response = response) => {
+  const { name, email, password } = request.body;
+
+  if ( name.length < 3 ) {
+    return response.status(400).json({
+      ok: false,
+      msg: "El nombre debe ser mayor a 2 caracteres",
+    });
+  }
+
   response.json({
     ok: true,
-    msg: "register"
+    msg: "register",
+    name,
+    email,
+    password
   });
 };
 
 
 const loginUsuario = (request, response = response) => {
+  const { email, password } = request.body;
+
   response.json({
     ok: true,
-    msg: "login"
+    msg: "login",
+    email,
+    password
   });
 };
 
